260800bb0e995618f6ad007eccc9cf7e15ca9c71
Patel Manav MineshKumar <138869553+manavPatelM@users.noreply.github.com>
2024-10-06 22:41:43 +0530

    💀

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from './node_modules/three';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
 
 // Set up scene, camera, and renderer
 let scene = new THREE.Scene();
